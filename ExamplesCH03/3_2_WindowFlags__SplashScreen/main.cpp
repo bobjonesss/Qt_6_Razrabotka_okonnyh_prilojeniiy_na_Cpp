63faa420a7d2c4c34b1e524b1d47742d2ffde7e9
bobjonesss <47592554+bobjonesss@users.noreply.github.com>
2024-01-22 16:25:59 +0200

    ExamplesCH03

---
+++ ExamplesCH03/3_2_WindowFlags__SplashScreen/main.cpp
@@ -0,0 +1,23 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+int main(int argc, char *argv[])
+{
+   QApplication app(argc, argv);
+   QWidget window;
+   window.setWindowTitle("Заголовок окна");
+   window.resize(350, 70);
+   window.setWindowFlags(Qt::SplashScreen);
+
+   QPushButton *btnQuit = new QPushButton("&Закрыть окно");
+   QVBoxLayout *vbox = new QVBoxLayout();
+   vbox->addWidget(btnQuit);
+   window.setLayout(vbox);
+   QObject::connect(btnQuit, SIGNAL(clicked()),
+                    &app, SLOT(quit()));
+
+   window.show();
+   return app.exec();
+}
---
+++ ExamplesCH03/3_2_WindowFlags__WindowMinimizeButtonHint/main.cpp
@@ -0,0 +1,24 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+int main(int argc, char *argv[])
+{
+   QApplication app(argc, argv);
+   QWidget window;
+   window.setWindowTitle("Заголовок окна");
+   window.resize(350, 70);
+   window.setWindowFlags(Qt::Window |
+                         Qt::WindowMinimizeButtonHint);
+
+   QPushButton *btnQuit = new QPushButton("&Закрыть окно");
+   QVBoxLayout *vbox = new QVBoxLayout();
+   vbox->addWidget(btnQuit);
+   window.setLayout(vbox);
+   QObject::connect(btnQuit, SIGNAL(clicked()),
+                    &app, SLOT(quit()));
+
+   window.show();
+   return app.exec();
+}
